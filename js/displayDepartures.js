e538d3c2f7d16befa619a61c26aa5da3702757f0
Datamagikeren <lasse4040@gmail.com>
2023-05-23 10:20:05 +0200

    add display departures

---
+++ js/displayDepartures.js
@@ -0,0 +1,33 @@
+const baseUrl2 = "http://localhost:5093/api/departures"
+
+const vueDepartures = Vue.createApp({
+    data() {
+        return {
+            departures: [],
+            noStudents: false
+        }
+    },
+    methods: {
+        async getAllDepartures() {
+            try {
+                const response = await axios.get(baseUrl2);
+                this.departures = await response.data;
+                this.noStudents = this.departures.length === 0;
+            } catch (ex) {
+                alert(ex.message);
+            }
+        },
+        formatTime(timeString) {
+            let date = new Date(timeString);
+            let day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+            let month = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1); // Months are zero indexed, hence the +1
+            let hours = (date.getHours() < 10 ? '0' : '') + date.getHours();
+            let minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+            return `${day}/${month} ${hours}:${minutes}`;
+          }
+          
+    },
+    // created() {
+    //     this.getAllDepartures();
+    // }
+}).mount("#vueDepartures");
